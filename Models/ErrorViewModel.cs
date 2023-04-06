0625dbaf207dbb6414e111de9d4dd357d3e62309
engelsruiz09 <jaruizc@correo.url.edu.gt>
2023-04-06 10:06:02 -0600

    agregado de clases nodo y arbol 2-3

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LAB04_ED1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TDA/Nodo23.cs
@@ -0,0 +1,13 @@
+﻿namespace TDA
+{
+    public class Nodo23<T> where T : IComparable<T>
+    {
+        public Nodo23<T> Lhijo { get; set; }//dato izquierdo
+        public Nodo23<T> Chijo { get; set; }//dato del medio
+        public Nodo23<T> Rhijo { get; set; }//dato de la derecha
+        public T VIzq { get; set; } //valor izquierdp
+        public T VDer { get; set; }//valor derecho
+
+
+    }
+}
